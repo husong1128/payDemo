a9127a962bef2d96eeb41b30714222c68d10dc7f
hxy <3358853260@qq.com>
2025-08-27 10:13:47 +0800

    add msg

--- wxPayDemo/src/main/java/com/wxpaydemo/WxPayDemoApplication.java
+++ wxPayDemo/src/main/java/com/wxpaydemo/WxPayDemoApplication.java
@@ -3,6 +3,9 @@ package com.wxpaydemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *  @author huxueyang,2025/8/27
+ */
 @SpringBootApplication
 public class WxPayDemoApplication {
 
